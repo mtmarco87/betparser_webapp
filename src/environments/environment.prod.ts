499702f6c89f28ee31f6d568deb125ccc81e2a44
EmanueleFricano <emanuele.FRICANO@amadeus.com>
2019-10-01 23:36:56 +0200

    adding firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {// Your web app's Firebase configuration
+    apiKey: 'AIzaSyBEL7WYzgT4UdvS4GaOtj_wKfnRDHQgxO4',
+    authDomain: 'parser-b8114.firebaseapp.com',
+    databaseURL: 'https://parser-b8114.firebaseio.com',
+    projectId: 'parser-b8114',
+    storageBucket: '',
+    messagingSenderId: '507364389957',
+    appId: '1:507364389957:web:4bd41fc6eaacee90efdf2c',
+    measurementId: 'G-NHSBF5BVNX'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {// Your web app's Firebase configuration
+    apiKey: 'AIzaSyBEL7WYzgT4UdvS4GaOtj_wKfnRDHQgxO4',
+    authDomain: 'parser-b8114.firebaseapp.com',
+    databaseURL: 'https://parser-b8114.firebaseio.com',
+    projectId: 'parser-b8114',
+    storageBucket: '',
+    messagingSenderId: '507364389957',
+    appId: '1:507364389957:web:4bd41fc6eaacee90efdf2c',
+    measurementId: 'G-NHSBF5BVNX'
+  }
 };
 
 /*
