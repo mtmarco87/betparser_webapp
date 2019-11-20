c167c90607658e03848c5824a0e9fd92eb777002
Marco <mt.marco87@gmail.com>
2019-11-20 23:36:07 +0100

    BetParser WebApp - Latest Updates

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# BetparserWebApp
+# BetParser WebApp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 6.0.8.
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,21 +1,20 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
   firebase: {// Your web app's Firebase configuration
-    apiKey: 'AIzaSyBEL7WYzgT4UdvS4GaOtj_wKfnRDHQgxO4',
-    authDomain: 'parser-b8114.firebaseapp.com',
-    databaseURL: 'https://parser-b8114.firebaseio.com',
-    projectId: 'parser-b8114',
+    apiKey: 'yourapikey',
+    authDomain: 'yourdomain',
+    databaseURL: 'yourdburl',
+    projectId: 'yourprojectid',
     storageBucket: '',
-    messagingSenderId: '507364389957',
-    appId: '1:507364389957:web:4bd41fc6eaacee90efdf2c',
-    measurementId: 'G-NHSBF5BVNX'
+    messagingSenderId: 'yourmessagingsenderid',
+    appId: 'yourappid',
+    measurementId: 'yourmeasurementid'
   }
 };
 
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
 /*
  * In development mode, to ignore zone related error stack frames such as
  * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
